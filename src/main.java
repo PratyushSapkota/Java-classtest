94067e358ce5bea901d0a720c8bdd52ec237963a
Pratyush Sapkota <pratyushsapkota@gmail.com>
2023-09-04 10:30:16 +0545

    Added loops

--- src/main.java
+++ src/main.java
@@ -20,72 +20,121 @@ public class main {
 
             if (userProgramSelection == 1) {
                 // calculator
-                System.out.println("Enter two numbers: ");
-                float firstNumber = sc.nextFloat();
-                float secondNumber = sc.nextFloat();
-                System.out.println("Enter an operator (+, -, /, *): ");
-                String selectedOperator = sc.next();
+                boolean IsContinued = true;
+                while (IsContinued) {
+                    System.out.println("Enter two numbers: ");
+                    float firstNumber = sc.nextFloat();
+                    float secondNumber = sc.nextFloat();
+                    System.out.println("Enter an operator (+, -, /, *): ");
+                    String selectedOperator = sc.next();
 
-                switch (selectedOperator) {
-                    case "+":
-                        System.out.println("Your result is: ");
-                        System.out.println(firstNumber + secondNumber);
+                    switch (selectedOperator) {
+                        case "+":
+                            System.out.println("Your result is: ");
+                            System.out.println(firstNumber + secondNumber);
+                            break;
+                        case "-":
+                            System.out.println("Your result is: ");
+                            System.out.println(firstNumber - secondNumber);
+                            break;
+                        case "*":
+                            System.out.println("Your result is: ");
+                            System.out.println(firstNumber * secondNumber);
+                            break;
+                        case "/":
+                            System.out.println("Your result is: ");
+                            System.out.println(firstNumber / secondNumber);
+                            break;
+                        default:
+                            System.out.println("Please select a valid operator.");
+                    }
+                    System.out.println("Do you want to exit? (y/n)");
+                    String Usercontinue = sc.next();
+
+                    if (Usercontinue.equals("y")) {
+                        System.out.println("Exiting the program.");
                         break;
-                    case "-":
-                        System.out.println("Your result is: ");
-                        System.out.println(firstNumber - secondNumber);
-                        break;
-                    case "*":
-                        System.out.println("Your result is: ");
-                        System.out.println(firstNumber * secondNumber);
-                        break;
-                    case "/":
-                        System.out.println("Your result is: ");
-                        System.out.println(firstNumber / secondNumber);
-                        break;
-                    default:
-                        System.out.println("Please select a valid operator.");
+                    } else if (!(Usercontinue.equals("y")) && !(Usercontinue.equals("n"))) {
+                        System.out.println("Invalid selection, not exiting.");
+                    }
                 }
 
 
             } else if (userProgramSelection == 2) {
                 // Guessing game
-                System.out.println("Enter your guess between 0 to 10");
-                int userGuess = sc.nextInt();
-                int randomNumber = (int) (Math.random() * 11);
 
-                if(randomNumber == userGuess){
-                    System.out.println("Your Guess was correct!");
-                }else {
-                    System.out.println("Your lost");
-                    System.out.println("The number you were supposed to guess was: " + randomNumber);
+
+                boolean IsContinued = true;
+                while (IsContinued) {
+                    System.out.println("Enter your guess between 0 to 10");
+                    int userGuess = sc.nextInt();
+                    int randomNumber = (int) (Math.random() * 11);
+
+                    if (randomNumber == userGuess) {
+                        System.out.println("Your Guess was correct!");
+                    } else {
+                        System.out.println("Your lost");
+                        System.out.println("The number you were supposed to guess was: " + randomNumber);
+                    }
+                    System.out.println("Do you want to exit? (y/n)");
+                    String Usercontinue = sc.next();
+                    if (Usercontinue.equals("y")) {
+                        System.out.println("Exiting the program.");
+                        break;
+                    } else if (!(Usercontinue.equals("y")) && !(Usercontinue.equals("n"))) {
+                        System.out.println("Invalid selection, not exiting.");
+                    }
                 }
 
 
             } else if (userProgramSelection == 3) {
                 // SI calculator
-                System.out.println("Enter principal: ");
-                float principal = sc.nextFloat();
-                System.out.println("Enter rate: ");
-                float rate = sc.nextFloat();
-                System.out.println("Enter time (in years): ");
-                float time = sc.nextFloat();
+                boolean IsContinued = true;
+                while (IsContinued) {
 
-                float SI = (principal * rate * time) / 100;
-                System.out.println("Your interest is: "+ SI);
-                System.out.println("Your total return is: " + (SI + principal));
+                    System.out.println("Enter principal: ");
+                    float principal = sc.nextFloat();
+                    System.out.println("Enter rate: ");
+                    float rate = sc.nextFloat();
+                    System.out.println("Enter time (in years): ");
+                    float time = sc.nextFloat();
+
+                    float SI = (principal * rate * time) / 100;
+                    System.out.println("Your interest is: " + SI);
+                    System.out.println("Your total return is: " + (SI + principal));
+                    System.out.println("Do you want to exit? (y/n)");
+                    String Usercontinue = sc.next();
+                    if (Usercontinue.equals("y")) {
+                        System.out.println("Exiting the program.");
+                        break;
+                    } else if (!(Usercontinue.equals("y")) && !(Usercontinue.equals("n"))) {
+                        System.out.println("Invalid selection, not exiting.");
+                    }
+                }
 
 
             } else if (userProgramSelection == 4) {
                 // vowel or consonant
-                System.out.println("Enter a letter: ");
-                String userInput = sc.next();
-                switch (userInput){
-                    case "a", "e", "i", "o", "u", "A", "E", "I", "O", "U":
-                        System.out.println("Vowel");
+                boolean IsContinued = true;
+                while (IsContinued) {
+
+                    System.out.println("Enter a letter: ");
+                    String userInput = sc.next();
+                    switch (userInput) {
+                        case "a", "e", "i", "o", "u", "A", "E", "I", "O", "U":
+                            System.out.println("Vowel");
+                            break;
+                        default:
+                            System.out.println("Consonant");
+                    }
+                    System.out.println("Do you want to exit? (y/n)");
+                    String Usercontinue = sc.next();
+                    if (Usercontinue.equals("y")) {
+                        System.out.println("Exiting the program.");
                         break;
-                    default:
-                        System.out.println("Consonant");
+                    } else if (!(Usercontinue.equals("y")) && !(Usercontinue.equals("n"))) {
+                        System.out.println("Invalid selection, not exiting.");
+                    }
                 }
 
 
